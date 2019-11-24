15da3b856b2c7cd643f3c24e03a3e115c980de43
davidmezzetti <561939+davidmezzetti@users.noreply.github.com>
2019-11-24 15:50:46 -0500

    Updated test script

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,6 +1,5 @@
-SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-SRC_DIR=$SCRIPTS_DIR/../src/python
-TEST_DIR=$SCRIPTS_DIR/../test/python
+SRC_DIR=./src/python
+TEST_DIR=./test/python
 
 export PYTHONPATH=$SRC_DIR:$TEST_DIR:$PYTHONPATH
 export PATH=$TEST_DIR:$PATH
